f1272f377b5e4b5c984166a8386d8854b0dfcf88
unknown <Ronel028>
2022-06-11 18:08:55 -0700

    Add function to nav button to display nav links

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,16 +2,21 @@
 // menu button
 let openBtn = document.querySelector('#open');
 let closeBtn = document.querySelector('#close');
+let navLinks = document.querySelector("#nav-links");
 
 openBtn.addEventListener('click', (e)=>{
     if(e.target.id === "open"){
         openBtn.style.display = "none";
         closeBtn.style.display = "block";
+        navLinks.style.opacity = '1';
+        navLinks.style.transform = "scale(1)"
     }
 })
 closeBtn.addEventListener('click', (e)=>{
     if(e.target.id === "close"){
         openBtn.style.display = "block";
         closeBtn.style.display = "none";
+        navLinks.style.opacity = "0";
+        navLinks.style.transform = "scale(0)"
     }
 })
